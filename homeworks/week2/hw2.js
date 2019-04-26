1ae8f70a5ce326acb262fe94a4e995c9396bb257
cynthia <b9808033@gmail.com>
2019-04-26 22:20:59 +0800

    hw2 eslint

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
-function capitalize(str) {
-  return str;
+let char = '';
+function capFirstStr(str) {
+  if (str[0] >= 'A' && str[0] <= 'Z') {
+    return str;
+  } if (str[0] >= 'a' && str[0] <= 'z') {
+    char = str[0].toUpperCase();
+    for (let i = 1; i <= str.length - 1; i += 1) {
+      char += str[i];
+    }
+    return char;
+  } return str;
 }
-
-console.log(capitalize('hello'));
+console.log(capFirstStr(',welcome'));
