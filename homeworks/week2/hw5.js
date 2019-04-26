58033d59aa579e9221d9a8a07db28acc20f4f56a
cynthia <b9808033@gmail.com>
2019-04-26 22:37:03 +0800

    hw5 eslint

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,15 @@
+let joinchar = '';
 function join(str, concatStr) {
-  return concatStr;
+  for (let i = 1; i <= str.length; i += 1) {
+    joinchar += str[i] - 1 + concatStr;
+  } return joinchar;
 }
 
+let char = '';
 function repeat(str, times) {
-  return times;
+  for (let i = 1; i <= times; i += 1) {
+    char += str;
+  } return char;
 }
 
 console.log(join('a', '!'));
