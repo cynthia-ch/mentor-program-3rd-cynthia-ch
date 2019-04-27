61cdbf94c6538a81802001ab23b089ca76dd6112
cynthia-ch <49459272+cynthia-ch@users.noreply.github.com>
2019-04-27 18:27:16 +0800

    Update hw5.js

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,7 +1,7 @@
 function join(str, concatStr) {
   let joinchar = '';
   for (let i = 1; i <= str.length; i += 1) {
-    joinchar += str[i] - 1 + concatStr;
+    joinchar += str[i - 1] + concatStr;
   } return joinchar;
 }
 
