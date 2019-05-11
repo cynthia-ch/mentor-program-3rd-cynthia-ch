bf233861a5b611261312a9da148c380ca40ea7cc
cynthia <b9808033@gmail.com>
2019-05-11 16:15:26 +0800

    hw4 eslint

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,14 @@
 function isPalindromes(str) {
-  console.log(str);
+  let result = '';
+  for (let i = 1; i <= str.length; i += 1) {
+    result += str[str.length - i];
+  }
+  if (result === str) {
+    return true;
+  }
+  return false;
 }
+
+console.log(isPalindromes('applppa'));
 
 module.exports = isPalindromes;
