abeb153bfcf6e334ac66b108e946a47b58cdee94
cynthia <b9808033@gmail.com>
2019-05-11 16:11:50 +0800

    hw2 eslint

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,17 @@
 function alphaSwap(str) {
-  console.log(str);
+  let result = '';
+  for (let i = 1; i <= str.length; i += 1) {
+    if (str[i - 1] <= 'z' && str[i - 1] >= 'a') {
+      result += str[i - 1].toUpperCase();
+    } else if (str[i - 1] <= 'Z' && str[i - 1] >= 'A') {
+      result += str[i - 1].toLowerCase();
+    } else {
+      result += str[i - 1];
+    }
+  }
+  return result;
 }
+
+console.log(alphaSwap(',Japan'));
 
 module.exports = alphaSwap;
